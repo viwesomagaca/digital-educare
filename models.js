062111d728151b350e977fc7fc53ce0c535405fa
Viwe <you@example.com>
2018-08-05 00:24:46 +0200

    Modifications

--- models.js
+++ models.js
@@ -11,7 +11,12 @@ module.exports = function(mongoUrl) {
         occupation: 'String',
         cell_no: 'Number',
         other:'String',
-        school_need: 'String'
+        school_need: 'String',
+        Instution: 'String',
+        Sector:'String',
+        Postal_code:'Number',
+        Urban_rural:'String',
+        Telephone:'Number',
     })
 
     return {
